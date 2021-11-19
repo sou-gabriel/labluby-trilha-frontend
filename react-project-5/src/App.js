5a5e163133b828ffcf3a6bba303a9107cd6ed7d9
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-11-19 13:49:37 -0300

    add react-project-5

---
+++ react-project-5/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button'
+import DemoOutput from './components/Demo/Demo';
+
+import './App.css';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false)
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph)
+  }
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-project-5/src/components/Demo/Demo.js
@@ -0,0 +1,7 @@
+import MyParagraph from "./MyParagraph"
+
+const DemoOutput = (props) => {
+  return <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph> 
+}
+
+export default React.memo(DemoOutput)
